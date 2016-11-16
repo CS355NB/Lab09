e72af6981f00819b6ed24f8611d0d8ca9864c095
Nazania <nbarraza12@gmail.com>
2016-11-16 14:42:37 -0800

    Finished Lab
    Lab09 (Web Application Development 101) Done

--- cs355/routes/index.js
+++ cs355/routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'CS355', subtitle: 'Lab 9' });
 });
 
+/* GET Template Example */
+router.get('/templatelink', function(req, res, next) {
+  res.render('templateexample.ejs', { title: 'cs355'});
+});
+
 module.exports = router;
